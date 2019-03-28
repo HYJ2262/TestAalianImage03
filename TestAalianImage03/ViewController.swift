448595bfedb88f411b84f04bc2f4ad2c9cc8a597
D7703_24 <d7703_24@d7703-24ui-imac.local>
2019-03-28 11:32:45 +0900

    Initial Commit

---
+++ TestAalianImage03/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAalianImage03
+//
+//  Created by D7703_24 on 2019. 3. 28..
+//  Copyright © 2019년 D7703_24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
